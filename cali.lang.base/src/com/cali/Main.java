3f3c3aa14d0db2b986b6efe72e3f1ad0295a1bb3
austin <austin@pop-os.localdomain>
2020-02-27 21:54:27 -0800

    Added apache commons cli support in Main class with -doc option.

--- cali.lang.base/src/com/cali/Main.java
+++ cali.lang.base/src/com/cali/Main.java
@@ -16,6 +16,11 @@
 
 package com.cali;
 
+import com.cali.stdlib.console;
+import org.apache.commons.cli.*;
+
+import java.util.List;
+
 /**
  * Main class implements program entry point for command line 
  * usage.
@@ -28,37 +33,91 @@ public class Main {
 	 * @throws Exception on failure.
 	 */
 	public static void main(String[] args) throws Exception {
-		if (args.length == 0) {
-			printUsage();
-			System.out.println("\nError, incorrect number of arguments. Expecting script file name.");
-		} else {
-			// Create a new Cali engine.
-			Engine eng = new Engine(new DefaultSecurityManagerImpl());
-			
-			// Sets debug output to true.
-			// eng.setDebug(true);
-			
-			// Add resource include path for testing.
-			eng.addResourceIncludePath("/com/cali/stdlib/ca/");
-			
-			// Parse the provided file name.
-			eng.parseFile(args[0]);
-			
-			// Prints the full parsed abastract syntax tree of all the sources.
-			//System.out.println(eng.toString());
-			
-			// Attempt to run the code.
-			eng.run();
+		// create the command line parser
+		CommandLineParser parser = new DefaultParser();
+
+		// create the Options
+		Options options = new Options();
+
+		// Help option
+		Option help = new Option("h", "help", false, "print this message" );
+		options.addOption(help);
+
+		// Calidoc option
+		Option doc = new Option("d", "doc", false, "generate calidoc for file");
+		options.addOption(doc);
+
+		try {
+			// parse the command line arguments
+			CommandLine line = parser.parse(options, args);
+
+			// has the buildfile argument been passed?
+			if(line.hasOption("help")) {
+				printHelp(options);
+				return;
+			} else if (line.hasOption("doc")) {
+				if (line.getArgList().size() == 1) {
+					String caliFile = line.getArgList().get(0);
+					console.get().info("Now to generate doc for file '" + caliFile + "'.");
+				} else if (line.getArgList().size() > 1) {
+					console.get().err("Too many arguments provided, expecting just one script file or none at all.\n");
+					printHelp(options);
+				}
+
+			} else {
+				List<String> pargs = line.getArgList();
+				if (pargs.size() == 0) {
+					console.get().err("Expecting cali script file, but no arguments found.\n");
+					printHelp(options);
+				} else if (pargs.size() > 1) {
+					console.get().err("Too many arguments provided, expecting just one script file.\n");
+					printHelp(options);
+				} else {
+					// We nailed it, let's run the file
+					String ScriptFile = pargs.get(0);
+					runFile(ScriptFile);
+				}
+			}
+		}
+		catch( ParseException exp ) {
+			System.out.println("CLI parse exception:" + exp.getMessage());
 		}
 	}
-	
+
+	public static void runFile(String ScriptFile) throws Exception {
+		// Create a new Cali engine.
+		Engine eng = new Engine(new DefaultSecurityManagerImpl());
+
+		// Sets debug output to true.
+		// eng.setDebug(true);
+
+		// Add resource include path for testing.
+		eng.addResourceIncludePath("/com/cali/stdlib/ca/");
+
+		// Parse the provided file name.
+		eng.parseFile(ScriptFile);
+
+		// Prints the full parsed abastract syntax tree of all the sources.
+		//System.out.println(eng.toString());
+
+		// Attempt to run the code.
+		eng.run();
+	}
+
+	public static void printHelp(Options options) {
+		// automatically generate the help statement
+		HelpFormatter formatter = new HelpFormatter();
+		printInfo();
+		formatter.printHelp("cali [options] <cali-file>", options);
+	}
+
 	/**
-	 * Prints the application usage to standard output.
+	 * Prints the application info to standard output.
 	 */
-	public static void printUsage() {
+	public static void printInfo() {
 		String rstr = "";
 		rstr += "Cali-Lang Version " + Universe.getCaliVersion()  + "\n";
-		rstr += "Copyright 2017 Austin Lehman\n";
+		rstr += "Copyright 2020 Austin Lehman\n";
 		rstr += "Apache License Version 2\n";
 		System.out.println(rstr);
 	}
